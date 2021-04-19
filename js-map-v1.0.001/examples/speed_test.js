9eca468623921947f81476711b61e231f617b3e3
Artexxx <54672403+Artexxx@users.noreply.github.com>
2021-04-19 22:36:22 +0300

    fix map style

--- js-map-v1.0.001/examples/speed_test.js
+++ js-map-v1.0.001/examples/speed_test.js
@@ -1,18 +1,3 @@
-/**
- * @fileoverview This demo is used for MarkerClusterer. It will show 100 markers
- * using MarkerClusterer and count the time to show the difference between using
- * MarkerClusterer and without MarkerClusterer.
- * @author Luke Mahe (v2 author: Xiaoxi Wu)
- */
-
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
 function $(element) {
   return document.getElementById(element);
 }
@@ -30,21 +15,23 @@ speedTest.init = function() {
   var options = {
     'zoom': 2,
     'center': latlng,
+    'mapId': "a71db9a0115114a1",
     'mapTypeId': google.maps.MapTypeId.ROADMAP
   };
 
   speedTest.map = new google.maps.Map($('map'), options);
   speedTest.pics = data.photos;
   
-  var useGmm = document.getElementById('usegmm');
+  var useGmm = $('usegmm');
   google.maps.event.addDomListener(useGmm, 'click', speedTest.change);
   
-  var numMarkers = document.getElementById('nummarkers');
+  var numMarkers = $('nummarkers');
   google.maps.event.addDomListener(numMarkers, 'change', speedTest.change);
 
   speedTest.infoWindow = new google.maps.InfoWindow();
 
   speedTest.showMarkers();
+  speedTest.ImportSearchLib()
 };
 
 speedTest.showMarkers = function() {
@@ -72,7 +59,7 @@ speedTest.showMarkers = function() {
     var item = document.createElement('DIV');
     var title = document.createElement('A');
     title.href = '#';
-    title.className = 'title';
+    title.className =  (speedTest.pics[i].active) ? 'title': 'title no_active';
     title.innerHTML = titleText;
 
     item.appendChild(title);
@@ -81,9 +68,12 @@ speedTest.showMarkers = function() {
 
     var latLng = new google.maps.LatLng(speedTest.pics[i].latitude,
         speedTest.pics[i].longitude);
-
-    var imageUrl = 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=' +
-        'FFFFFF,008CFF,000000&ext=.png';
+    
+    var imageUrl = (speedTest.pics[i].active) ? 
+      /* синий маркер */   'http://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=FFFFFF,008CFF,000000&ext=.png'
+      :
+      /* красный маркер */ 'http://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=FFFFFF,ff008c,000000&ext=.png';
+    
     var markerImage = new google.maps.MarkerImage(imageUrl,
         new google.maps.Size(24, 32));
 
@@ -93,9 +83,9 @@ speedTest.showMarkers = function() {
     });
 
     var fn = speedTest.markerClickFunction(speedTest.pics[i], latLng);
-    google.maps.event.addListener(marker, 'click', fn);
-    google.maps.event.addDomListener(title, 'click', fn);
-    speedTest.markers.push(marker);
+      google.maps.event.addListener(marker, 'click', fn);
+      google.maps.event.addDomListener(title, 'click', fn);
+      speedTest.markers.push(marker);
   }
 
   window.setTimeout(speedTest.time, 0);
@@ -113,14 +103,19 @@ speedTest.markerClickFunction = function(pic, latlng) {
     var url = pic.photo_url;
     var fileurl = pic.photo_file_url;
 
-    var infoHtml = '<div class="info"><h3>' + title +
-      '</h3><div class="info-body">' +
-      '<a href="' + url + '" target="_blank"><img src="' +
-      fileurl + '" class="info-img"/></a></div>' +
-      '<a href="LOGO.png" target="_blank">' +
-      '<img src="LOGO.png"/></a><br/>' +
-      ' Ссылка: <a href="' + pic.owner_url + '" target="_blank">' + pic.owner_name +
-      '</a></div></div>';
+    var infoHtml = 
+    `<div class="info">
+      <h3> ${title}</h3>
+      <div class="info-body">
+        <a href="${url}" target="_blank">
+          <img src="${fileurl}" class="info-img"/>
+        </a>
+      </div>
+      <a href="LOGO.png" target="_blank">
+        <img src="LOGO.png"/>
+      </a><br/>
+      Ссылка: <a href="${pic.owner_url}" target="_blank">${pic.owner_name}</a>
+    </div>`;
 
     speedTest.infoWindow.setContent(infoHtml);
     speedTest.infoWindow.setPosition(latlng);
@@ -150,7 +145,44 @@ speedTest.time = function() {
       marker.setMap(speedTest.map);
     }
   }
-
   var end = new Date();
   $('timetaken').innerHTML = end - start;
 };
+
+speedTest.ImportSearchLib = function() {
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+   $('pac-input'));
+  speedTest.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // For each place, get the icon, place name, and location.
+    // markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      // Create a marker for each place.
+      // position: place.geometry.location
+      bounds.extend(place.geometry.location);
+    }
+
+    speedTest.map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
+
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(speedTest.map, 'bounds_changed', function() {
+    var bounds = speedTest.map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+}
